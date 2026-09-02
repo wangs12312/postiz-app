b5de1868eba6ea57dda9ca4ecb503cc440979bf2
wangs12312 <wangs@yishuibh.com>
2026-09-02 19:51:51 +0800

    11

--- apps/frontend/src/app/(app)/(site)/launches/page.tsx
+++ apps/frontend/src/app/(app)/(site)/launches/page.tsx
@@ -1,11 +1,12 @@
 export const dynamic = 'force-dynamic';
 import { LaunchesComponent } from '@gitroom/frontend/components/launches/launches.component';
 import { Metadata } from 'next';
-import { isGeneralServerSide } from '@gitroom/helpers/utils/is.general.server.side';
+
 export const metadata: Metadata = {
-  title: `${isGeneralServerSide() ? 'Postiz Calendar' : 'Gitroom Launches'}`,
+  title: 'YishuiBH Calendar',
   description: '',
 };
+
 export default async function Index() {
   return <LaunchesComponent />;
 }
